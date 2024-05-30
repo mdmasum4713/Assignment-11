ac56eb00a3992f210264c91db2c28f5914e7a64a
mdmasum4713 <mdmasum4713@gmail.com>
2024-05-30 17:34:00 +0600

    first commit

--- src/pages/ExtraSection/ExtraSection.jsx
+++ src/pages/ExtraSection/ExtraSection.jsx
@@ -1,17 +1,72 @@
-import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 
 
 const ExtraSection = () => {
     return (
-        <div className='app'>
-            <MessengerCustomerChat
-                pageId="354986437692421"
-                appId="284992384603993"
+        <section className="dark:bg-gray-100 dark:text-gray-800">
+        <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+            <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50">
+                <img src="https://i.ibb.co/G7gvrNd/images-1.jpg" alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500" />
+                <div className="p-6 space-y-2 lg:col-span-5">
 
-            />,
-            
+                    <p>Volunteering, often seen as the backbone of a thriving community, plays a pivotal role in</p>
+                </div>
+            </a>
+            <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
+                    <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://i.ibb.co/0jhgzSV/istockphoto-1303107115-612x612.jpg" />
+                    <div className="p-6 space-y-2">
+                        <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">Volunteering</h3>
+                        <p>Some volunteer resources include JustServe, which lists volunteer needs of organizations, and volunteers search for opportunities to enhance the quality of life in.</p>
+                    </div>
+                </a>
+                <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
+                    <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://i.ibb.co/rsnN4M9/360-F-32069160-96-Jp-OLq-NYK15-MBN3-UPl-XBp-Zjuj3-Hy-Gqx.jpg" />
+                    <div className="p-6 space-y-2">
+                        <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline"> Boost to Local Economie</h3>
+                        <span className="text-xs dark:text-gray-600">January 22, 2021</span>
+                        <p>Volunteers often contribute skills and time, which can be particularly beneficial in areas lacking specific resources or financial capabilities</p>
+                    </div>
+                </a>
+                <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
+                    <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://i.ibb.co/0jhgzSV/istockphoto-1303107115-612x612.jpg" />
+                    <div className="p-6 space-y-2">
+                        <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">Career Development</h3>
+                        <span className="text-xs dark:text-gray-600">January 23, 2021</span>
+                        <p>For volunteers, the experience gained can be immensely valuable in career progression. It offers a platform for acquiring new skills, </p>
+                    </div>
+                </a>
+                <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
+                    <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://i.ibb.co/ckhrRY5/Short-term.jpg" />
+                    <div className="p-6 space-y-2">
+                        <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">ISocial Benefits</h3>
+                        <span className="text-xs dark:text-gray-600">January 24, 2021</span>
+                        <p>MVolunteering fosters a sense of community by bringing people together for common goals. It breaks down social barriers, encouraging .</p>
+                    </div>
+                </a>
+                <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
+                    <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://i.ibb.co/ckhrRY5/Short-term.jpg" />
+                    <div className="p-6 space-y-2">
+                        <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline"> Enhancing Social Welfare:</h3>
+                        <span className="text-xs dark:text-gray-600">January 25, 2021</span>
+                        <p>By addressing community needs directly, volunteering efforts significantly enhance social welfare. Programs that focus on education, health,.</p>
+                    </div>
+                </a>
+                <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50 hidden sm:block">
+                    <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="https://i.ibb.co/rsnN4M9/360-F-32069160-96-Jp-OLq-NYK15-MBN3-UPl-XBp-Zjuj3-Hy-Gqx.jpg" />
+                    <div className="p-6 space-y-2">
+                        <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">Promoting Civic Engagement: </h3>
+                        <span className="text-xs dark:text-gray-600">January 26, 2021</span>
+                        <p> Volunteers are often more informed and involved in civic activities. By participating in volunteering activities, individuals gain a deeper.</p>
+                    </div>
+                </a>
+            </div>
+            <div className="flex justify-center">
+                <button type="button" className="px-6 py-3 text-sm rounded-md hover:underline dark:bg-gray-50 dark:text-gray-600">Load more posts...</button>
+            </div>
         </div>
+      
+    </section>
     );
 };
 
